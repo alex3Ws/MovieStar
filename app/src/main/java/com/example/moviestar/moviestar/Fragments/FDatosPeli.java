778a81d62a5b91b2579cb8cdc3eeeccdcd3aa9a5
Unknown <alejandro.rosa.gail@gmail.com>
2018-05-16 01:04:30 +0200

    Fragment DatosPeli DONE

--- app/src/main/java/com/example/moviestar/moviestar/Fragments/FDatosPeli.java
+++ app/src/main/java/com/example/moviestar/moviestar/Fragments/FDatosPeli.java
@@ -1,6 +1,7 @@
 package com.example.moviestar.moviestar.Fragments;
 
 import android.content.Context;
+import android.icu.text.UnicodeSetSpanner;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -8,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -25,10 +27,9 @@ import org.json.JSONObject;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class FDatosPeli extends Fragment {
+public class FDatosPeli extends Fragment implements Response.Listener<JSONObject>, Response.ErrorListener{
 
     View vista;
-    Pelicula pelicula;
     RequestQueue request;
     JsonObjectRequest jsonObjectRequest;
     TextView titulo_original,compañias,generos,pais,actores,sinopsis;
@@ -55,6 +56,8 @@ public class FDatosPeli extends Fragment {
         }
 
 
+        request = Volley.newRequestQueue(getContext());
+
         titulo_original= vista.findViewById(R.id.tvTituloOriginal2);
         compañias = vista.findViewById(R.id.tvCompañias2);
         generos = vista.findViewById(R.id.tvGenero2);
@@ -69,7 +72,12 @@ public class FDatosPeli extends Fragment {
 
     private void cargarDatos() {
 
-        titulo_original.setText(jsonObject.optString("original_title"));
+
+        if(jsonObject.has("original_title") && !jsonObject.optString("original_title").equals("") && jsonObject.optString("original_title") != null) {
+            titulo_original.setText(jsonObject.optString("original_title"));
+        }else{
+            titulo_original.setText("Unknown");
+        }
 
         JSONArray jsonArrayComp = jsonObject.optJSONArray("production_companies");
 
@@ -80,19 +88,22 @@ public class FDatosPeli extends Fragment {
 
         try {
 
-            for (i = 0; i < jsonArrayComp.length(); i++) {
+            if(jsonArrayComp.length() > 0) {
+                for (i = 0; i < jsonArrayComp.length(); i++) {
 
-                json = null;
-                json = jsonArrayComp.getJSONObject(i);
+                    json = null;
+                    json = jsonArrayComp.getJSONObject(i);
 
-                if(i != (jsonArrayComp.length() - 1))
-                    texto = texto + json.optString("name") + ", ";
-                else
-                    texto = texto + json.optString("name");
+                    if (i != (jsonArrayComp.length() - 1))
+                        texto = texto + json.optString("name") + ", ";
+                    else
+                        texto = texto + json.optString("name");
+                }
+
+                compañias.setText(texto);
+            }else {
+                compañias.setText("Unknown");
             }
-
-            compañias.setText(texto);
-
         } catch(JSONException e){
             e.printStackTrace();
             compañias.setText("No se han podido recuperar las compañias");
@@ -104,20 +115,26 @@ public class FDatosPeli extends Fragment {
         texto = "";
 
         try {
+            if(jsonArrayGen.length() > 0) {
+                for (i = 0; i < jsonArrayGen.length(); i++) {
 
-            for(i=0;i<jsonArrayGen.length();i++){
+                    json = null;
+                    json = jsonArrayGen.getJSONObject(i);
 
-                json = null;
-                json = jsonArrayGen.getJSONObject(i);
+                    if (i != (jsonArrayGen.length() - 1))
+                        texto = texto + json.optString("name") + ", ";
+                    else
+                        texto = texto + json.optString("name");
 
-                if(i != (jsonArrayGen.length() - 1))
-                    texto = texto + json.optString("name") + ", ";
-                else
-                    texto = texto + json.optString("name");
+                }
 
+                generos.setText(texto);
+
+            }else{
+               generos.setText("Unknown");
             }
 
-            generos.setText(texto);
+
 
         } catch(JSONException e){
             e.printStackTrace();
@@ -128,32 +145,88 @@ public class FDatosPeli extends Fragment {
         texto = "";
 
         try {
+            if(jsonArrayPais.length() > 0) {
+                for (i = 0; i < jsonArrayPais.length(); i++) {
 
-            for(i=0;i<jsonArrayPais.length();i++){
+                    json = null;
+                    json = jsonArrayPais.getJSONObject(i);
 
-                json = null;
-                json = jsonArrayPais.getJSONObject(i);
+                    if (i != (jsonArrayPais.length() - 1))
+                        texto = texto + json.optString("name") + ", ";
+                    else
+                        texto = texto + json.optString("name");
 
-                if(i != (jsonArrayPais.length() - 1))
-                    texto = texto + json.optString("name") + ", ";
-                else
-                    texto = texto + json.optString("name");
+                }
 
+                pais.setText(texto);
+
+            }else{
+                pais.setText("Unknown");
             }
 
-            pais.setText(texto);
 
         } catch(JSONException e){
             e.printStackTrace();
             generos.setText("No se han podido recuperar los paises");
         }
 
+        if(jsonObject.has("overview") && !jsonObject.optString("overview").equals("") && jsonObject.optString("overview") != null) {
+            sinopsis.setText(jsonObject.optString("overview"));
+        }
+        else{
+            sinopsis.setText("Unknown");
+        }
 
-        sinopsis.setText(jsonObject.optString("overview"));
+
+        String url = "http://api.themoviedb.org/3/movie/"+jsonObject.optInt("id")+"/casts?api_key=a2424ed363ead46acaa726cf8cb45bad";
+
+        jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,url,null,this,this);
+
+        request.add(jsonObjectRequest);
+    }
+
+    @Override
+    public void onResponse(JSONObject response) {
+
+        try {
+            String texto ="";
+            int cont = 0;
+            JSONArray jsonArray = response.optJSONArray("cast");
+        if(jsonArray.length() > 0) {
+            for (int i = 0; i < jsonArray.length(); i++) {
+
+                JSONObject json = jsonArray.getJSONObject(i);
+
+                if ((json.optInt("gender") == 1 || json.optInt("gender") == 2) && json.optString("profile_path") != null) {
+
+                    if (i == (jsonArray.length() - 1) || cont == 14) {
+                        texto = texto + json.optString("name");
+                    } else {
+                        texto = texto + json.optString("name") + ", ";
+                    }
+                    cont++;
+                }
+
+                if (cont == 15) {
+                    i = jsonArray.length();
+                }
+            }
+            actores.setText(texto);
+        }else{
+            actores.setText("Unknown");
+        }
 
 
 
 
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+    @Override
+    public void onErrorResponse(VolleyError error) {
+        Toast.makeText(getContext(),"Algo ha ido mal", Toast.LENGTH_SHORT).show();
     }
 
 
